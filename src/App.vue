bfd6b624ae5d25e589564082da25cca00a40820a
Rory <rory88@gmail.com>
2017-01-10 18:41:21 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+
+    <ul>
+      <li v-for="(person, index) in people">
+        {{ person }} - {{ index }}
+      </li>
+    </ul>
+
+    <div v-for="(value, key, index) in catStats">
+      <div>{{ key }}: {{ value }} - {{ index }}</div>
+    </div>
+
+    <div v-for="n in amountOfHorizontalRules">
+      {{ n }}: <hr />
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        people: ['Grace', 'Tim', 'Sergey'],
+        catStats: {
+          name: 'Opal',
+          breed: 'Grey Tortoiseshell',
+          favoriteThing: 'sleeping'
+        },
+        amountOfHorizontalRules: 5
+      }
+    }
+  }
+</script>
